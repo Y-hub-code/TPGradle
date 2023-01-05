54f8fec4884777504a6067dc8771c2c36bad6140
Y-hub-code <etisalatte1421@gmail.com>
2023-01-05 23:09:47 +0100

    Connexion

---
+++ src/main/java/com/telly/controllers/UserController.java
@@ -0,0 +1,86 @@
+package com.telly.controllers;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.telly.dao.User;
+import com.telly.dao.FormValidationGroup ;
+import com.telly.service.UserService;
+
+
+@Controller
+public class UserController {
+
+
+
+	@RequestMapping("/login")
+	public String showLogin() {
+		return "login";
+	}
+	@RequestMapping("/loggedout")
+	public String showLogout() {
+		return "loggedout";
+	}
+
+
+	@RequestMapping("/createaccount")
+	public String createAccount(Model model, Principal principal) {
+
+		model.addAttribute("user", new User());
+
+		return "createaccount";
+	}
+
+	@RequestMapping(value = "/getreservations", method = RequestMethod.GET)
+	public String getReserveBook(@Validated(FormValidationGroup.class) Reserve reserve, Model model, Principal principal) {
+
+
+		String username = principal.getName();
+		reserve.getUser().setUsername(username);
+
+		List<Reserve> reserves = reserveService.getReserves(username);
+		model.addAttribute("reserves", reserves);
+		System.out.println(reserves);
+
+
+		return "home";
+
+	}
+
+	@RequestMapping(value = "/reservebook", method = RequestMethod.POST)
+	public String createReserveBook(@Validated(FormValidationGroup.class) Reserve reserve, BindingResult result, Principal principal) {
+
+		if (result.hasErrors()) {
+			return "reservebus";
+		}
+
+		String username = principal.getName();
+		reserve.getUser().setUsername(username);
+
+		reserveService.reserve(reserve);
+
+
+		return "home";
+
+	}
+	@RequestMapping(value = "/createuser", method = RequestMethod.POST)
+	public String createUser(@Validated(FormValidationGroup.class) User user, BindingResult result) {
+
+		if(result.hasErrors()) {
+			return "createaccount";
+		}
+
+		user.setAuthority("ROLE_USER");
+		user.setEnabled(true);
+
+		userService.create(user);
+
+		return "home";
+
+	}
+
+}
+
+
+
